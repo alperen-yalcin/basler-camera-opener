d95daeae69b07ad0ed49ad721e8199d3e8dfc6e0
Arge Stajyer <argestajyer1@ferel.local>
2025-07-28 16:19:58 +0300

    feat(camera): open camera and start grabbing images

--- cam_video_capture.py
+++ cam_video_capture.py
@@ -1,15 +1,17 @@
 from pypylon import pylon
 
-target_serial = "24068739"
-
+target_serial = "12345678"
 available_cameras = pylon.TlFactory.GetInstance().EnumerateDevices()
-
 camera_found = False
+
 for cam_info in available_cameras:
     if cam_info.GetSerialNumber() == target_serial:
-        print(f"Camera found: {target_serial}")
+        camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateDevice(cam_info))
+        camera.Open()
+        camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+        print("The camera turned on and started capturing images.")
         camera_found = True
         break
 
 if not camera_found:
-    print("No camera with specified serial number found.")
+    print("No camera with specified serial number.")
