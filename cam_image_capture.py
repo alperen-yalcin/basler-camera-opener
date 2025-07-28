ceaadbbab3f660abca2001fcf8aca22b8beb8323
Arge Stajyer <argestajyer1@ferel.local>
2025-07-28 14:33:10 +0300

    feat(camera): implement camera selection by serial number

--- cam_image_capture.py
+++ cam_image_capture.py
@@ -2,3 +2,14 @@ from pypylon import pylon
 import cv2
 
 target_serial = '24068739'
+
+def get_camera_by_serial(serial_number):
+    available_cameras = pylon.TlFactory.GetInstance().EnumerateDevices()
+    for cam_info in available_cameras:
+        if cam_info.GetSerialNumber() == serial_number:
+            camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateDevice(cam_info))
+            camera.Open()
+            print(f'Camera turned on: {camera.GetDeviceInfo().GetModelName()}')
+            return camera
+    print('No camera with specified serial number found.')
+    return None
